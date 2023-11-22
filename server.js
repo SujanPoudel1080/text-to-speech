b34ecce6c0e4e60e58f8ecda2e30524e002cc7d8
SujanPoudel1080 <sujanpoudel1080@gmail.com>
2023-11-22 16:04:56 +0545

    initialized frontend

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 import express from "express";
+import say from "say";
 
 const app = express();
 
 app.listen(9000, () => {
   console.log("server running at port 9000");
+});
+
+const text = "I live in Nepal";
+
+const tts = new say.Say("darwin");
+
+tts.speak(text, "Alex", 1, (err) => {
+  if (err) {
+    return console.error(err);
+  }
+  console.log("text to speech complete");
 });
