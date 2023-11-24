9930b6e9fe4e9f0227b5d7c4ea8ced8219883459
SujanPoudel1080 <sujanpoudel1080@gmail.com>
2023-11-24 05:47:57 +0545

    set up cheerio in backend

--- server.js
+++ server.js
@@ -1,22 +1,35 @@
 import express from "express";
-import say from "say";
+import axios from "axios";
+import * as cheerio from "cheerio";
+// import say from "say";
 
 const app = express();
+// const sec = <section class='story-section'><p></p></section>;
 
 app.listen(9000, () => {
   console.log("server running at port 9000");
 });
+const url = "https://kathmandupost.com/karnali-province/2023/11/23";
+const fetchData = async (url) => {
+  const { data } = await axios.get(url);
+  const $ = cheerio.load(data);
+  const newsItem = $("section").find('p');
+  console.log(newsItem);
+};
 
-const text = "hello!";
+fetchData(url);
 
-const tts = new say.Say("darwin");
+// const text = "sumarga is a good boy";
 
-tts.speak(text, "Alex", 1, (err) => {
-  if (err) {
-    return console.error(err);
-  }
-//   const list = tts.getInstalledVoices();
-console.log("successfully converted text to speech");
+// const tts = new say.Say("darwin");
 
-  process.exit();
-});
+// tts.speak(text, "Rishi", 1, (err) => {
+//   if (err) {
+//     console.error(err);
+//     process.exit();
+//   }
+//   //   const list = tts.getInstalledVoices();
+//   console.log("successfully converted text to speech");
+
+//   process.exit();
+// });
